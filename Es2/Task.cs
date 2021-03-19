1fc7566f5c5b0ecd4540773a90db77c99771d854
Giorgio Vecchi <gvecchi@hotmail.com>
2021-03-19 13:46:09 +0100

    Add project files.

---
+++ Es2/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Es2
+{
+    enum Formato
+    {
+        Plain,
+        CSV
+    }
+
+    class Task
+    {
+        public int ID { get; }
+        public string Descrizione { get; }
+        public DateTime DataScadenza { get; }
+        public Livello Livello { get; }
+
+        public Task(int id, string descrizione, DateTime dataScadenza, Livello livello)
+        {
+            if (dataScadenza < DateTime.Today)
+                throw new ArgumentOutOfRangeException();
+
+            ID = id;
+            Descrizione = descrizione;
+            DataScadenza = dataScadenza;
+            Livello = livello;
+        }
+
+        public string OttieniDati(Formato formato)
+        {
+            switch (formato)
+            {
+                case Formato.Plain:
+                    return $"ID: {ID}, {Descrizione}, Scadenza: {DataScadenza.ToShortDateString()}, Livello: {Livello}";
+                case Formato.CSV:
+                    return $"{ID};{Descrizione};{DataScadenza.ToShortDateString()};{Livello}";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Es2/TaskManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Es2
+{
+    enum Livello
+    {
+        Basso,
+        Medio,
+        Alto,
+        Tutti
+    }
+
+    class TaskManager
+    {
+        int _id;    // progressivo da attribuire a ogni task creato
+        Dictionary<int, Task> _tasks = new Dictionary<int, Task>();
+        public Task CreaTask(string descrizione, DateTime dataScadenza, Livello livello)
+        {
+            Task t = new Task(++_id, descrizione, dataScadenza, livello);
+
+            _tasks[t.ID] = t;
+
+            return t;
+        }
+
+        public bool EsisteTask(int id)
+        {
+            return _tasks.ContainsKey(id);
+        }
+
+        public void EliminaTask(int id)
+        {
+            _tasks.Remove(id);
+        }
+
+        public string OttieniElenco(Livello livello, Formato formato)
+        {
+            string s = "";
+
+            foreach (Task t in _tasks.Values)
+                if (livello == Livello.Tutti || t.Livello == livello)
+                    s += t.OttieniDati(formato) + '\n';
+
+            return s;
+        }
+    }
+}
